62b61f79cfe1ad4fb496960d7b3ac1678312665f
Fermín Olaiz <ferminolaiz@gmail.com>
2014-10-22 20:24:29 -0300

    Show results & Fixed some errors

--- examen.php
+++ examen.php
@@ -1,57 +1,67 @@
 <?php
 	class Examen
 	{
-		private $File = 'wireless.html';
+		private $File = 'examen.html';
 		private $Answers = array
 		(
 			array
 			(
 				'q' => 'Pregunta?',
+				'r' => 'Respuesta',
 				'a' => 1
 			),
 			array
 			(
 				'q' => 'Pregunta?',
+				'r' => 'Respuesta',
 				'a' => 1
 			),
 			array
 			(
 				'q' => 'Pregunta?',
+				'r' => 'Respuesta',
 				'a' => 1
 			),
 			array
 			(
 				'q' => 'Pregunta?',
+				'r' => 'Respuesta',
 				'a' => 1
 			),
 			array
 			(
 				'q' => 'Pregunta?',
+				'r' => 'Respuesta',
 				'a' => 1
 			),
 			array
 			(
 				'q' => 'Pregunta?',
+				'r' => 'Respuesta',
 				'a' => 1
 			),
 			array
 			(
 				'q' => 'Pregunta?',
+				'r' => 'Respuesta',
 				'a' => 1
 			),
 			array
 			(
 				'q' => 'Pregunta?',
+				'r' => 'Respuesta',
 				'a' => 1
 			),
 			array
 			(
 				'q' => 'Pregunta?',
+				'r' => 'Respuesta',
 				'a' => 1
 			),
 			array
 			(
 				'q' => 'Pregunta?',
+				'r' => 'Respuesta',
 				'a' => 1
 			)
 		);
@@ -60,9 +70,6 @@
 		{
 			try
 			{
-				if(count($Answers) != count($this->Answers))
-					return -1;
-
 				$Points = 0;
 				$Errors = array();
 
@@ -73,7 +80,7 @@
 					if($Answers[$i] == $this->Answers[$i]['a'])
 						$Points++;
 					else
-						array_push($Errors, array(array($Answers[$i], $this->Answers[$i])));
+						array_push($Errors, array($Answers[$i], $this->Answers[$i]['q'], $this->Answers[$i]['r']));
 				}
 
 				return array
@@ -84,8 +91,60 @@
 			}
 			catch (Exception $E)
 			{
-				// LOG ERROR
-				header("Location: {$this->File}");
+				// Log
+				return array('redirect' => $this->File);
 			}
 		}
 	}
+
+	if(isset($_POST['1']) && isset($_POST['2']) && isset($_POST['3']) && isset($_POST['4']) && isset($_POST['5']) && isset($_POST['6']) && isset($_POST['7']) && isset($_POST['8']) && isset($_POST['9']) && isset($_POST['10']))
+	{
+		$E = new Examen();
+		$R = $E->Check
+		(
+			array
+			(
+				$_POST['1'],
+				$_POST['2'],
+				$_POST['3'],
+				$_POST['4'],
+				$_POST['5'],
+				$_POST['6'],
+				$_POST['7'],
+				$_POST['8'],
+				$_POST['9'],
+				$_POST['10']
+			)
+		);
+
+		if(isset($R['points']) && isset($R['errors']))
+		{
+			if($R['points'] == 10 && $R['errors'] === array())
+			{
+				echo 'Puntaje: 10/10';
+				echo '<br>';
+				echo '¡Felicitaciones! No has cometido ningún error';
+			}
+			else
+			{
+				echo "Puntaje: {$R['points']}/10";
+				echo '<br>';
+				echo 'Has cometido algunos errores: ';
+
+				foreach($R['errors'] as $Error)
+				{
+					echo '<br>';
+					echo "{$Error[0]}. {$Error[1]}";
+					echo '<br>';
+					echo "Respuesta correcta: {$Error[2]}";
+				}
+			}
+		}
+		else if(isset($R['redirect']))
+			header("Location: {$R['redirect']}");
+	}
+	else
+		header('Location:examen.html?e=error'); //FIX HARDCODE
+
+	// TODO: 
+	// Change header for echo;*/
