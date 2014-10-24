34b25a14639940d2524781b9346497bbbe81a1b9
Fermín Olaiz <ferminolaiz@gmail.com>
2014-10-24 20:29:21 -0300

    Fixed HTML (To show with results) & <?php is <?=

--- examen.php
+++ examen.php
@@ -117,7 +117,21 @@
 		)
 	);
 	###### END CONFIG ######
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Exámen Wireless | Underc0de</title>
 
+		<meta charset="UTF-8">
+
+		<meta name="author" content="fermino">
+		<meta name="description" content="Exámen online en el área de Wireless. Seguridad, Hacking, etc...">
+		<meta name="keywords" content="wireless, hacking, cracking, examen">
+	</head>
+	<body>
+		<h1><?=$TestName ?> | Underc0de</h1>
+<?php
 	if(isset($_POST['0']) && isset($_POST['1']) && isset($_POST['2']) && isset($_POST['3']) && isset($_POST['4']) && isset($_POST['5']) && isset($_POST['6']) && isset($_POST['7']) && isset($_POST['8']) && isset($_POST['9']))
 	{
 		$E = new Underc0dest($TestName, $Answers);
@@ -172,40 +186,27 @@
 	else
 	{
 ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Exámen Wireless | Underc0de</title>
-
-		<meta charset="UTF-8">
-
-		<meta name="author" content="fermino">
-		<meta name="description" content="Exámen online en el área de Wireless. Seguridad, Hacking, etc...">
-		<meta name="keywords" content="wireless, hacking, cracking, examen">
-	</head>
-	<body>
-		<h1><?php echo $TestName ?> | Underc0de</h1>
-		<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
+		<form action="<?=$_SERVER['REQUEST_URI']?>" method="post">
 <?php
 			$ic = count($Answers);
 			for($i = 0; $i < $ic; $i++)
 			{
 ?>
-			<h3><?php echo $Answers[$i]['q']; ?></h3>
+			<h3><?=$Answers[$i]['q']?></h3>
 <?php
 				$jc = count($Answers[$i]['r']);
 				for($j = 0; $j < $jc; $j++)
 				{
 ?>
-			<input type="radio" name="<?php echo $i; ?>" value="<?php echo $j; ?>"><?php echo $Answers[$i]['r'][$j]; ?><br>
+			<input type="radio" name="<?=$i; ?>" value="<?php echo $j; ?>"><?php echo $Answers[$i]['r'][$j]?><br>
 <?php
 				}
 			}
 ?>
 			<input type="submit" name="submit" value="Listo!">
 		</form>
-	</body>
-</html>
 <?php
 	}
 ?>
+</body>
+</html>
