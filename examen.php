5ebfce15c1d3b2650afb40e0277837015e9f2d5d
Fermín Olaiz <ferminolaiz@gmail.com>
2014-10-26 18:45:40 -0300

    Added function to close floating div & Startdust :3

--- examen.php
+++ examen.php
@@ -1,5 +1,9 @@
 <?php
-	require_once 'underc0dest.class.php';
+	if(is_file('underc0dest.class.php'))
+		require_once 'underc0dest.class.php';
+	else
+		exit;
+
 	if(isset($TestName) && isset($Answers))
 	{
 ?>
@@ -7,31 +11,20 @@
 <html>
 	<head>
 		<title><?=$TestName?> | Underc0de</title>
-
 		<meta charset="UTF-8">
-
-		<link rel="stylesheet" type="text/css" media="all" href="style.css">
-
-		<script>
-			function Send()
-			{
-				// VALIDAR RADIOS
-				// ENVIAR
-			}
-		</script>
+		<link rel="stylesheet" type="text/css" media="all" href="resources/style.css">
+		<script src="resources/underc0dest.js"></script>
 	</head>
 	<body>
 	<div align="center">
 		<a href="http://undercde.org/foro/" title="Volver al foro...">
-			<img class="cabecera" src="http://underc0de.org/foro/Themes/underblack/images/theme/logo.png">
-			<br>
-			<span>Volver al foro...</span>
+			<img src="http://underc0de.org/foro/Themes/underblack/images/theme/logo.png">
 		</a>
 		<hr>
 	</div>
-	<h2><?=$TestName ?></h2>
-	<br>
+	<h1><?=$TestName ?></h1>
 	<div id="flotante">
+		<img id="HideBoxImg" src="resources/close.png" onClick="HideBox()">
 		<p>Mas que una comunidad informática Underc0de es una gran familia de la que formas parte fundamental, aquí las ideas fluyen libres, los conocimientos van y vienen en todas direcciones, pero necesitamos de ti para hacerlo posible. Anímate completando el siguiente ********** (formulario, test, examen, no se...) Te sonríes un poco y al mismo tiempo nos ayudas a enfocar nuestros materiales en beneficio de ustedes los underc0ders.</p>
 		<a href="http://underc0de.org/" title="Volver al foro...">Volver al foro...</a>
 	</div>
@@ -63,29 +56,28 @@
 			if(isset($R['points']) && isset($R['errors']))
 			{
 ?>
-		Puntaje: <?=$R['points']?>/10
-		<br>
+		<h3>Puntaje: <?=$R['points']?>/10</h3>
 <?php
 				if($R['points'] == 10 && $R['errors'] === array())
 				{
 ?>
-		¡Felicitaciones! No has cometido ningún error
+		<h4>¡Felicitaciones! No has cometido ningún error</h4>
 <?php
 				}
 				else
 				{
 ?>
-		Has cometido algunos errores: 
+		<h4>Has cometido algunos errores: </h4>
 <?php
 					foreach($R['errors'] as $Error)
 					{
 ?>
-		<br>
+		<br><br>
 		<?=$Error[0]?>. <?=$Error[1]/*FIXEAR NUMEROS*/?> 
 		<br>
-		Has respondido: <?=$Error[2]?>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Has respondido: <?=$Error[2]?>
 		<br>
-		Respuesta correcta: <?=$Error[3]?>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Respuesta correcta: <?=$Error[3]?>
 <?php
 					}
 				}
@@ -108,7 +100,7 @@
 					for($j = 0; $j < $jc; $j++)
 					{
 ?>
-			<input type="radio" name="<?=$i; ?>" value="<?php echo $j; ?>"><?php echo $Answers[$i]['r'][$j]?><br>
+			<input type="radio" name="<?=$i; ?>" value="<?php echo $j; ?>">&nbsp;<?php echo $Answers[$i]['r'][$j]?><br>
 <?php
 					}
 				}
