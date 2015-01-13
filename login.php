585c36648f121d897459958aca4ead1877f3d9d4
Fermín Olaiz <fermino.github@gmail.com>
2015-01-13 16:03:21 -0300

    try-catch deleted from login

--- login.php
+++ login.php
@@ -1,20 +1,13 @@
 <?php
-	try
+	error_reporting(0);
+
+	if(!empty($_POST['username']))
 	{
-		error_reporting(0);
-		if(isset($_POST['username']))
-		{
-			session_start();
-			$_SESSION['underc0dest_uname'] = $_POST['username'];
-			exit('true');
-		}
-		else
-			exit('false');
-	}
-	catch (Exception $E)
-	{
-		error_log($E->getMessage() . NL, 3, 'error_log');
-		exit('false');
+		session_start();
+
+		$_SESSION['underc0dest_uname'] = $_POST['username'];
+
+		exit('true');
 	}
 
-	exit;
+	exit('false');
